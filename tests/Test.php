4883a1fac0b4042620d3d00768b26bbac1c47c14
gotfunc <benoit.villiere@gmail.com>
2019-03-10 14:45:44 +0100

    Initial commit

---
+++ tests/Test.php
@@ -0,0 +1,17 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
+{
+    public function setUp()
+    {
+        //
+    }
+
+    /** @test */
+    public function testSuiteIsWorkingProperly()
+    {
+        $this->assertTrue(true);
+    }
+}
